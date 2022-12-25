69102806f893f41e41a8c67f49fd16edd6b3346e
rangerboy574 <jdeweese574@gmail.com>
2022-12-25 17:11:37 -0600

    Initial Commit

---
+++ DailyWork/DailyWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWorkApp.swift
+//  DailyWork
+//
+//  Created by Joseph  DeWeese on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
